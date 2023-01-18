d367ab2e89fa9aa37dead6c20688d1a8a39223ee
sharmila <sharmilamuhzin@gmail.com>
2023-01-18 11:20:56 +0530

    api for deprtment:TA-21

--- controllers/department.controller.js
+++ controllers/department.controller.js
@@ -1,10 +1,36 @@
-import {department} from '../services/department.sevice.js'
+import {deleteData, department, getAll, update} from '../services/department.sevice.js'
 
-export async function createDepartmentList(req,res,next){
+export async function DepartmentList(req,res,next){
     let departmentName = req.body.departmentName
     try {
         const departmentList = await department({departmentName})
         res.send(departmentList)
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+export async function getDepartmentList(req,res){
+    const { result } = await getAll()
+    res.send(result)
+}
+
+export async function updateDepartment(req,res,next){
+    try{
+        const updatedDepartment = await update(req,res)
+        res.send(updatedDepartment)
+    
+    }catch(error){
+        next(error)
+    }
+   
+}
+
+export async function deleteDepartment(req,res){
+    try {
+        const deleteDprtmnt = await deleteData(req.params.id)
+        res.send(deleteDprtmnt)
     } catch (error) {
         next(error)
     }
--- services/department.sevice.js
+++ services/department.sevice.js
@@ -1,7 +1,27 @@
-import departmentModel from "../models/department.model.js"
+import departmentModel from "../models/department.model.js";
 
-export async function department(departmentName){
-    const department = new departmentModel(departmentName)
-    await department.save()
-    return { department }
+export async function department(departmentName) {
+  const department = new departmentModel(departmentName);
+  await department.save();
+  return { department };
+}
+
+export async function getAll() {
+  const result = await departmentModel.find({});
+  return { result };
+}
+
+export async function update(req,res) {
+  const updateData = await departmentModel.findByIdAndUpdate(
+    req.params.id,
+    { departmentName: req.body.departmentName },
+    { new: true }
+  );
+  return { updateData };
+}
+
+
+export async function deleteData(id){
+    const deletedData = await departmentModel.findByIdAndDelete(id)
+    return{deletedData}
 }
